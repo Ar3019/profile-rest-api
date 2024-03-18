62c191925064a1636da07bd67b6c43f1373b669f
Arundhathi <barundhathi99@gmail.com>
2024-03-18 15:18:41 +0530

    Created model Viewset for Profile Objects

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from profiles_api import serializers
+from profiles_api import models 
 
 # Create your views here.
 
@@ -92,4 +93,8 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk = None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
-    
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
